2dc0500c0c4bbda3d98d3dcf6d04f50595fb5046
xinzhedeai <2291006094>
2018-01-31 18:02:29 +0800

    轮播图

---
+++ 华为商城/index.js
@@ -0,0 +1,17 @@
+/**
+ * @author xinzhedeai
+ * @date  2018年1月31日09:28:07
+ */
+'use strict';
+
+ var mySwiper = new Swiper ('.swiper-container', {
+    direction: 'horizontal',
+    loop: true,
+    
+    // 如果需要分页器
+    pagination: {
+      el: '.swiper-pagination',
+    }
+    
+  })        
+
